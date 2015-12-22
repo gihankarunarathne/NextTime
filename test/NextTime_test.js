d24d9dc5f84d509fb711157a84748648dd0dbe45
Gihan Karunarathne <gckarunarathne@gmail.com>
2015-12-22 15:36:33 +0530

    Implemented nextDates() & nextWeeks(), add test cases
    - Added test cases
    - Implemented methods separately from nextDate() and nextWeek() methods,
      in order to increase performance gain by removing date validation

--- test/NextTime_test.js
+++ test/NextTime_test.js
@@ -2,7 +2,7 @@
 
 let assert = require('assert');
 let NextTime = require('../index').NextTime;
-let debug = require('debug')('ScheduleForecaster:test:NextTime');
+let debug = require('debug')('NextTime:test:NextTime');
 
 describe('NextTime ', () => {
     let nextTime = null;
@@ -26,6 +26,27 @@ describe('NextTime ', () => {
         });
     });
 
+    describe('NextDates ', () => {
+        it('should return array of dates ', done => {
+            const dates =
+                nextTime.nextDates(new Date(2015, 11, 1), new Date(2016, 0, 1));
+            assert.ok(dates[0] instanceof Date);
+            done();
+        });
+
+        it('should return next dates ', done => {
+            const dates =
+                nextTime.nextDates(new Date(2015, 11, 1), new Date(2016, 0, 1));
+            debug('NextDates : ', dates.toString());
+            assert.equal(dates.length, 31);
+            assert.deepEqual(dates[0], new Date(2015,11,1));
+            assert.deepEqual(dates[1], new Date(2015,11,2));
+            assert.deepEqual(dates[2], new Date(2015,11,3));
+            assert.deepEqual(dates[dates.length -1], new Date(2015,11,31));
+            done();
+        });
+    });
+
     describe('NextWeek ', () => {
         it('should return next week date object ', done => {
             const d = nextTime.nextWeek();
@@ -36,6 +57,23 @@ describe('NextTime ', () => {
         it('should return next week ', done => {
             const d = nextTime.nextWeek(new Date(2015, 11, 17));
             assert.deepEqual(d, new Date(2015, 11, 24));
+            done();
+        });
+    });
+
+    describe('NextWeeks ', () => {
+        it('should return next weeks date object ', done => {
+            const dates =
+                nextTime.nextWeeks(new Date(2015, 11, 1), new Date(2015, 11, 8));
+            assert.ok(dates[0] instanceof Date);
+            done();
+        });
+
+        it('should return next weeks ', done => {
+            const weeks =
+                nextTime.nextWeeks(new Date(2015, 11, 1), new Date(2016, 0, 1));
+            debug('Weeks : ', weeks);
+            assert.equal(weeks.length, 5);
             done();
         });
     });
